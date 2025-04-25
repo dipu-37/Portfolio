a61df112f4615e42041cbe175352c9dd608838fe
Razibul Hasan Dipu <razibul1105nstu@gmail.com>
2025-04-25 11:34:47 +0600

    fix image path for varcel

--- src/data/recentWorks.js
+++ src/data/recentWorks.js
@@ -3,18 +3,18 @@ export const recentWorks = [
     title: "Personal Portfolio",
     description: "Professional page for personal portfolio showcase.",
     techs: ["Tailwind", "JS", "Vite"],
-    img: "/public/image/work.png",
+    img: "/image/work.png",
   },
   {
     title: "Doctors Portal",
     description: "Doctors appointment booking website.",
     techs: ["React", "Node", "JS"],
-    img: "/public/image/work.png",
+    img: "/image/work.png",
   },
   {
     title: "Digital Marketing",
     description: "Professional page for digital marketing.",
     techs: ["BS5", "CSS", "JS"],
-    img: "/public/image/work.png",
+    img: "/image/work.png",
   },
 ];
